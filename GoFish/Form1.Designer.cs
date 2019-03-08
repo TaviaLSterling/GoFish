c0ee066a45c5140e53a7f36aaecb6a88901502a8
Tavia Sterling <TaviaSterling@outlook.com>
2019-03-08 14:35:38 -0700

    form design

--- GoFish/Form1.Designer.cs
+++ GoFish/Form1.Designer.cs
@@ -28,12 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textName = new System.Windows.Forms.TextBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textName
+            // 
+            this.textName.Location = new System.Drawing.Point(50, 26);
+            this.textName.Name = "textName";
+            this.textName.Size = new System.Drawing.Size(186, 22);
+            this.textName.TabIndex = 0;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(300, 24);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(142, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "Start The Game!";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 499);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.textName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textName;
+        private System.Windows.Forms.Button buttonStart;
     }
 }
 
